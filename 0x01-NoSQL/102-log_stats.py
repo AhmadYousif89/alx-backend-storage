3c0a3a3c5edf2fe593364b04c53e047a0df4dae3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-13 01:15:43 +0300

    Add log_stats function to retrieve log statistics

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Adding the top 10 of the most present IPs in the collection nginx of the database logs"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Log stats"""
+    client = MongoClient()
+    logs = client.logs.nginx
+    print(f"{logs.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    print(f"{logs.count_documents({'path': '/status'})} status check")
+
+    print("IPs:")
+    ips = logs.aggregate(
+        [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+        ]
+    )
