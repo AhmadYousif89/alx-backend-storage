af534c90ff7444f7c382bc69076c925999963464
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-14 18:24:53 +0300

    Add get_str and get_int methods to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,10 +27,10 @@ class Cache:
 
     def get_str(self, key: str) -> str:
         """Get the value from the Redis database as string"""
-        value = self._redis.get(key)
-        return value.decode("utf-8") if value else ""
+        value: bytes | None = self._redis.get(key)
+        return value.decode("utf-8") if value else "(nil)"
 
     def get_int(self, key: str) -> int:
         """Get the value from the Redis database as integer"""
-        value = self._redis.get(key)
-        return int(value) if value else 0
+        value: bytes | None = self._redis.get(key)
+        return int(value.decode("utf-8")) if value else 0
