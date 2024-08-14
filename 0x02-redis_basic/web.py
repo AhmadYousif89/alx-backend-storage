7b6db04406b96aeab0996f6bf06c69ce11bd62b6
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-15 00:02:58 +0300

    Refactor Redis cache variable name and update method calls

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,21 +6,21 @@ from functools import wraps
 import requests
 import redis
 
-r = redis.Redis()
+cache = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
     """Decorator that increments the count for a URL"""
 
     @wraps(method)
-    def wrapper(url):
+    def wrapper(url):  # sourcery skip: use-named-expression
         """Wrapper for decorator"""
-        r.incr(f"count:{url}")
-        cached_html = r.get(f"cached:{url}")
+        cache.incr(f"count:{url}")
+        cached_html = cache.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
         html = method(url)
-        r.setex(f"cached:{url}", 10, html)
+        cache.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
