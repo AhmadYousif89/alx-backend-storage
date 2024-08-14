b2050e8a263fda14a38eb2f29da359ded1f1b942
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-14 18:51:21 +0300

    Update Redis usage in exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,19 +20,24 @@ import redis
 
 
 class Cache:
-    """Cache class to store data in Redis"""
+    '''
+    Cache class.
+    '''
 
     def __init__(self):
-        """Initialize the Redis connection"""
+        '''
+        Initialize the cache.
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    # @count_calls
-    def store(self, data: Union[str, int, bytes, float]) -> str:
-        """Store the value in the Redis database and return the key"""
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''
+        Store data in the cache.
+        '''
+        randomKey = str(uuid4())
+        self._redis.set(randomKey, data)
+        return randomKey
 
     # def get(
     #     self, key: str, fn: Optional[Callable] = None
