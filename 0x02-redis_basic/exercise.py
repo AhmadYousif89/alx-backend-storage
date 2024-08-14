c4c4ce54e5cfce39877126686c9dcd621e3d850c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-14 19:04:59 +0300

    Refactor cache key generation in store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,17 +6,29 @@ from functools import wraps
 from uuid import uuid4
 import redis
 
+# def count_calls(method: Callable) -> Callable:
+#     """Decorator to count the number of calls to a method"""
+
+#     @wraps(method)
+#     def wrapper(self, *args, **kwargs):
+#         """Wrapper function"""
+#         self._redis.incr(method.__qualname__)
+#         return method(self, *args, **kwargs)
+
+#     return wrapper
+
 
 class Cache:
-    '''Cache class.'''
+    """Cache class to store data in Redis"""
 
     def __init__(self):
-        '''Initialize the cache.'''
+        """Initialize the Redis connection"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Store data in the cache.'''
+    # @count_calls
+    def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Store the value in the Redis database and return the key"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
